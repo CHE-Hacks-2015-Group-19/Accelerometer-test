077ba746da95f45a4044a0cceac8c857a2bc03ed
atf1999 <torth212@gmail.com>
2015-11-07 17:15:30 -0500

    Accelerometer test code

--- Accelerometer/app/src/main/java/crash/beacon/MainActivity.java
+++ Accelerometer/app/src/main/java/crash/beacon/MainActivity.java
@@ -1,14 +1,52 @@
 package crash.beacon;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    Sensor accel;
+    SensorManager manager;
+    float x = 0, y = 0, z = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        manager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accel = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        manager.registerListener(this, accel, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        manager.registerListener(this, accel, SensorManager.SENSOR_DELAY_NORMAL);
+
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        manager.unregisterListener(this);
+    }
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+   // can be safely ignored for this demo
+    }
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        x = event.values[0];
+        y = event.values[1];
+        z = event.values[2];
+
+        Log.d("X", String.valueOf(x));
+        Log.d("Y", String.valueOf(y));
+        Log.d("Z", String.valueOf(z));
+
+
+
     }
 }
